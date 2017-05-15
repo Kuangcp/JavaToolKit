b868c8b84f310b62f52430c9ec513ac89e391b5a
mythos <kuangchengping@outlook.com>
2017-05-15 21:24:03 +0800

    成功配置了REST的basepath

--- src/test/java/net/HttpRequestTest.java
+++ src/test/java/net/HttpRequestTest.java
@@ -18,7 +18,7 @@ public class HttpRequestTest {
         System.out.println("返回结果；"+result);
         time.End("");
         time.Start();
-        result = HttpRequest.sendGet("https://www.bing.com","q=89");
+        result = HttpRequest.sendGet("http://localhost:8888/myth/test/redis","q=89");
         System.out.println("返回结果；"+result);
         time.End("");
     }
