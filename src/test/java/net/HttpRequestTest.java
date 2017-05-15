9a92261e2e26e1dbcddc7c932b7ce5df704f39a2
mythos <kuangchengping@outlook.com>
2017-05-15 16:41:32 +0800

    为了简化security的配置，只好使用一张表来进行登录操作，然后登录的时候，不注销就一直属于在线，使用了cookie？不下线的？

---
+++ src/test/java/net/HttpRequestTest.java
@@ -0,0 +1,25 @@
+package net;
+
+import com.myth.net.HttpRequest;
+import com.myth.time.GetRunTime;
+import org.junit.Test;
+
+/**
+ * Created by mythos on 17-5-15
+ * By kuangchengping@outlook.com
+ */
+public class HttpRequestTest {
+
+    @Test
+    public void testRun(){
+        GetRunTime time = new GetRunTime();
+        time.Start();
+        String result = HttpRequest.sendGet("https://www.bing.com","");
+        System.out.println("返回结果；"+result);
+        time.End("");
+        time.Start();
+        result = HttpRequest.sendGet("https://www.bing.com","q=89");
+        System.out.println("返回结果；"+result);
+        time.End("");
+    }
+}
