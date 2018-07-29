9b90be64008b9d48eaa0e3d7f137a7c38ecf9f7c
mythos <kuangcp@aliyun.com>
2018-07-29 23:25:16 +0800

    update pom.xml

--- src/main/java/com/github/kuangcp/time/DateFormatUtils.java
+++ src/main/java/com/github/kuangcp/time/DateFormatUtils.java
@@ -10,20 +10,25 @@ import java.util.Date;
 public class DateFormatUtils {
 
   private static SimpleDateFormat format = new SimpleDateFormat();
+  private static String YYYY_MM_DD_HH_MM_SS = "YYYY-MM-dd HH:MM:SS";
+  private static String YYYY_MM_DD_HH_MM = "YYYY-MM-dd HH:MM";
 
   /**
    * YYYY-MM-dd HH:MM:SS
    */
   public static String toYMDHMS(Date date) {
-    format.applyPattern("YYYY-MM-dd HH:MM:SS");
-    return format.format(date);
+    return format(date, YYYY_MM_DD_HH_MM_SS);
   }
 
   /**
    * YYYY-MM-dd HH:MM
    */
   public static String toYMDHM(Date date) {
-    format.applyPattern("YYYY-MM-dd HH:MM");
+    return format(date, YYYY_MM_DD_HH_MM);
+  }
+
+  public static String format(Date date, String pattern) {
+    format.applyPattern(pattern);
     return format.format(date);
   }
 
